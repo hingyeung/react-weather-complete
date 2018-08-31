37963fa3c79c384fced57d75c87769f1286718d9
Samuel Li <samli@samuelli.net>
2018-08-31 10:08:49 +1000

    added brief intruction on how to use the location selector and credit.

--- src/components/ui/LocationSelector.tsx
+++ src/components/ui/LocationSelector.tsx
@@ -1,15 +1,25 @@
 import LocationPicker from "react-location-picker";
 import * as React from "react";
 import { ILocationSelectorProps } from "../../types";
+import styled from 'styled-components';
+
+const LSInstruction = styled.p`
+  font-size: small;
+  color: darkgrey;
+  text-align: center;
+`;
 
 const LocationSelector: React.SFC<ILocationSelectorProps> = (props) => {
   return (
-    <LocationPicker
-      containerElement={ <div /> }
-      mapElement={ <div style={ {height: '400px'} } /> }
-      onChange={props.onLocationChange}
-      defaultPosition={props.defaultPosition}
-    />
+    <div>
+      <LocationPicker
+        containerElement={ <div /> }
+        mapElement={ <div style={ {height: '400px'} } /> }
+        onChange={props.onLocationChange}
+        defaultPosition={props.defaultPosition}
+      />
+      <LSInstruction>Drag marker to select new location. By <a href="https://github.com/rameshsyn/react-location-picker">rameshsyn</a>.</LSInstruction>
+    </div>
   )
 };
 
