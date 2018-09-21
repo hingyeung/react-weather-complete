74b3bbe4781624c84607dc8284724aeeb771c580
Samuel Li <samli@samuelli.net>
2018-09-21 12:07:43 +1000

    added api key for google geocoder

--- src/services/GeocodeService.ts
+++ src/services/GeocodeService.ts
@@ -3,7 +3,7 @@ import Location from "./Location";
 const axios = require('axios');
 class GeocodeService {
   reverseGeocode(location: Location) {
-    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?latlng=${location.lat},${location.lon}`, {timeout: 5000});
+    return axios.get(`https://maps.googleapis.com/maps/api/geocode/json?key=AIzaSyB3lFMeUQ3ZYY8aFbuK_BTJ_Oq1L2u0ev0&latlng=${location.lat},${location.lon}`, {timeout: 5000});
   }
 }
 
