0f997adb74a11f63524168c41853a9c728be53b5
Samuel Li <samli@samuelli.net>
2019-11-28 23:43:35 +1100

    updated doco

--- README.md
+++ README.md
@@ -1,21 +1,27 @@
 ## react-weather-combo
 
 ### Install Dependencies
-AWS CDK  
-`npm install -g aws-cdk`  
-Lerna  
-`npm install -g lerna`
+AWS CDK & Lerna  
+`> npm install -g aws-cdk lerna`  
+`> lerna bootstrap`  
+
+### Setup environment config
+`> cp .env-template .env.test`  
+Modify .env.test accordingly.
 
 ### Useful Tasks
 Build artefact for production environment  
-`> lenra build`  
+`> lenra run build`  
 
 Build artefactor for test environment  
-`> lerna build:test`  
+`> lerna run build:test`  
+
+Show generated CF template
+`> CDK_STACK_NAME=react-weather-test AWS_PROFILE=your_profile lerna run cdk:synth:test`
 
 Deploy stack `react-weather-test` to test environment  
-`> CDK_STACK_NAME=react-weather-test lerna cdk:deploy:test`  
+`> CDK_STACK_NAME=react-weather-test AWS_PROFILE=your_profile lerna run cdk:deploy:test`  
 
 Destroy stack `react-weather-dev` (I couldn't get the `cdk destroy` command working with the `lerna`)  
-`> cd packages/ui-infra && CDK_STACK_NAME=react-weather-dev yarn run cdk:destroy:test`  
+`> cd packages/ui-infra && CDK_STACK_NAME=react-weather-dev AWS_PROFILE=your_profile yarn run cdk:destroy:test`  
 
--- packages/ui-infra/lib/s3-webhosting-stack.ts
+++ packages/ui-infra/lib/s3-webhosting-stack.ts
@@ -1,54 +1,7 @@
 import cdk = require('@aws-cdk/core');
 import s3 = require('@aws-cdk/aws-s3');
-import { BlockPublicAccess } from '@aws-cdk/aws-s3';
-import {CloudFrontWebDistribution, CfnCloudFrontOriginAccessIdentity} from '@aws-cdk/aws-cloudfront';
 import SinglePageAppHosting from './single-page-app-hosting';
 import {HostedZone} from '@aws-cdk/aws-route53';
-
-// export class S3WebhostingStack extends cdk.Stack {
-//   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
-//     super(scope, id, props);
-
-//     // https://docs.aws.amazon.com/cdk/latest/guide/get_context_var.html
-//     // https://stackoverflow.com/questions/58138536/how-to-specify-parameter-definition-in-cdk-stack
-//     // Context can be set using "-c"
-//     // e.g. cdk synth -c bucket_name=mygroovybucket
-//     const sourceBucketName = this.node.tryGetContext("bucket_name");
-//     const certArn = this.node.tryGetContext("cert_arn");
-//     if (! sourceBucketName) {
-//       throw Error('bucket_name is not specified');
-//     }
-
-//     const sourceBucket = new s3.Bucket(this, 's3bucket', {
-//       bucketName: sourceBucketName,
-//       // blockPublicAccess: BlockPublicAccess.BLOCK_ALL,
-//       websiteIndexDocument: 'index.html',
-//       // publicReadAccess: true
-//     });
-
-//     const cfOriginAccessIdentity = new CfnCloudFrontOriginAccessIdentity(this, 'OAI', {
-//       cloudFrontOriginAccessIdentityConfig: {
-//         comment: cdk.Aws.STACK_NAME
-//       }
-//     });
-
-//     const distribution = new CloudFrontWebDistribution(this, `${sourceBucketName}-cfront`, {
-//       originConfigs: [{
-//           s3OriginSource: {
-//             s3BucketSource: sourceBucket,
-//           },
-//           behaviors: [{isDefaultBehavior: true}]
-//       }],
-//       viewerCertificate: {
-//         aliases: [sourceBucketName],
-//         props: {
-//           acmCertificateArn: certArn,
-//           sslSupportMethod: 'sni-only'
-//         }
-//       }
-//     })
-//   }
-// }
 
 export interface S3WebhostingStackProps extends cdk.StackProps {
   CDK_DOMAIN_NAME: string;
