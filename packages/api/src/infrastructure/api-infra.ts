3e184af11b1851ac6577a83612687832cb667288
Samuel Li <samli@samuelli.net>
2019-12-05 12:12:49 +1100

    added API gateway with CDK

--- packages/api/src/infrastructure/api-infra.ts
+++ packages/api/src/infrastructure/api-infra.ts
@@ -1,7 +1,23 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import cdk = require('@aws-cdk/core');
-import {ApiStack} from './api-stack';
+import {ApiStack, ApiStackProps} from './api-stack';
+import dotenv = require('dotenv');
 
-const app = new cdk.App();
-const ignored = new ApiStack(app, 'ApiInfraStack');
+const stackEnv = process.env.CDK_STACK_ENV;
+const stackName = process.env.CDK_STACK_NAME;
+const envConfig = { ...dotenv.config({path: `.env.${stackEnv}`}).parsed} as ApiStackProps;
+
+if (!stackEnv || !stackName) {
+  console.error(`CDK_STACK_ENV and CDK_STACK_NAME environment variables must be set.`);
+  process.exit(1);
+} else {
+  const app = new cdk.App();
+  const ignored = new ApiStack(app, stackName, {
+    ...envConfig,
+    env: {
+      account: process.env.CDK_DEFAULT_ACCOUNT, 
+      region: process.env.CDK_DEFAULT_REGION
+    }
+  });
+}
--- packages/api/src/infrastructure/api-stack.ts
+++ packages/api/src/infrastructure/api-stack.ts
@@ -2,32 +2,44 @@ import cdk = require('@aws-cdk/core');
 import {Function, Runtime, Code} from '@aws-cdk/aws-lambda';
 import {join} from 'path';
 import dotenv = require('dotenv');
+import { RestApi, LambdaIntegration } from '@aws-cdk/aws-apigateway';
 
 interface EnvConfig {
   [DARK_SKY_API_KEY: string]: string;
 }
 
-const stackEnv = process.env.CDK_STACK_ENV;
-const stackName = process.env.CDK_STACK_NAME;
-const envConfig = {...dotenv.config({path: `.env.${stackEnv}`}).parsed} as
-    EnvConfig;
+export interface ApiStackProps extends cdk.StackProps {
+  DARK_SKY_API_KEY: string;
+}
 
 export class ApiStack extends cdk.Stack {
-  constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
+  constructor(scope: cdk.Construct, id: string, props: ApiStackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // lambda function
     const weatherApiFunction = new Function(this, 'weatherApiFunction', {
       runtime: Runtime.NODEJS_10_X,
       handler: 'index.handler',
       code: Code.asset(join(__dirname, '..')),
-      environment: {DARK_SKY_API_KEY: envConfig.DARK_SKY_API_KEY},
+      environment: {DARK_SKY_API_KEY: props.DARK_SKY_API_KEY},
     });
 
     // Output the function name, which is needed when invoking the function
     // using `sam local invoke "function_logical_id_from_generated_cf_template"`.
     // Hoping for better integration between CDK and SAM soon:
     // https://github.com/aws/aws-cdk/issues/716
-    new cdk.CfnOutput(this, "weatherApiFunctionOutput", { value: weatherApiFunction.functionName});
+    const ignored = new cdk.CfnOutput(this, "weatherApiFunctionOutput", { value: weatherApiFunction.functionName});
+
+    // api gateway
+    const api = new RestApi(this, 'api', {
+      deployOptions: {
+        stageName: 'dev'
+      }
+    });
+    const weather = api.root.addResource("weather");
+    const weatherMethod = weather.addMethod(
+      'GET',
+      new LambdaIntegration(weatherApiFunction)
+    );
   }
 }
